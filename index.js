a39775bcbff0f64bab19a2b3c0a36001b28ad5ad
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-07-31 14:55:19 +0300

    add rules in eslint and add tasks

--- index.js
+++ index.js
@@ -5,8 +5,40 @@ const mapper = {
   'Leia': 'sister',
   'R2D2': 'droid',
   'Han': 'brother in law',
-}
-
-const solution = (person) => {
-  return `Luke, I am your ${mapper[person]}.`;
 };
+
+const solution1 = (person) => `Luke, I am your ${mapper[person]}.`;
+
+console.log(solution1('Leia'));
+
+// Task: squares_and_cubes
+
+const solution2 = (first, second) => {
+  const sqrt = Math.sqrt(first);
+  const cbrt = Math.cbrt(second);
+
+  if (sqrt === cbrt) {
+    return true;
+  }
+
+  return false;
+};
+
+console.log(solution2(4, 8));
+
+// Task: smallest_second_number
+
+const solution3 = (numbers) => {
+  const sort = numbers.sort((a, b) => a - b);
+  return sort[1];
+};
+
+console.log(solution3([1, 2, 3]));
+
+// Task: minmax_find
+
+const solution4 = (numbers) => [Math.min(...numbers), Math.max(...numbers)];
+
+console.log(solution4([-3, 2, 10, 22]));
+
+// Закончил на 31.07 14:02
