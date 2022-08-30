b8b31167eeee5be06f87153ee1db21c5731defbe
Ratmir Kalimulin <kalimulin.91@bk.ru>
2022-08-30 22:33:07 +0300

    add rules in eslint & add tasks

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 
 const mapper = {
   'Darth Vader': 'father',
-  'Leia': 'sister',
-  'R2D2': 'droid',
-  'Han': 'brother in law',
+  Leia: 'sister',
+  R2D2: 'droid',
+  Han: 'brother in law',
 };
 
 const solution1 = (person) => `Luke, I am your ${mapper[person]}.`;
@@ -41,4 +41,45 @@ const solution4 = (numbers) => [Math.min(...numbers), Math.max(...numbers)];
 
 console.log(solution4([-3, 2, 10, 22]));
 
-// Закончил на 31.07 14:02
+// Task: reverse_each_word
+
+const solution5 = (s) => {
+  let str = '';
+  for (let i = s.length - 1; i >= 0; i -= 1) {
+    str += s[i];
+  }
+
+  return str.split(' ').reverse().join(' ');
+};
+
+console.log(solution5('Hello there, and how are you?'));
+
+// Task: celsius_to_kelvin
+
+const solution6 = (temperature) => temperature + 273;
+
+console.log(solution6(35));
+
+// Task: cube_sum
+
+const solution7 = (num) => {
+  let res = 0;
+  for (let i = 0; i <= num; i++) {
+    res += i ** 3;
+  }
+
+  return res;
+};
+
+console.log(solution7(20));
+
+// Task: fizzbuzz
+
+const solution8 = (n) => {
+  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+  if (n % 3 === 0) return 'Fizz';
+  if (n % 5 === 0) return 'Buzz';
+  return '';
+};
+
+console.log(solution8(150));
